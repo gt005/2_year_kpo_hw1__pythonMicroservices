11651c1ab1d52ec524b71bb34b9399d0c2026d32
Karim <karimhamid@yandex.ru>
2023-05-29 01:49:43 +0300

    Добавлены коллекции Postman для тестов, а также добавлена документация в README.md

--- Order_handler_service/api_app/apps.py
+++ Order_handler_service/api_app/apps.py
@@ -5,11 +5,3 @@ from django.apps import AppConfig
 class ApiAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api_app'
-
-
-# class RunAsyncTasks(AppConfig):
-#     name = 'api_app'
-#
-#     def ready(self):
-#         from .async_tasks.order_status_switcher_task import switch_next_order_status
-#         switch_next_order_status.apply_async(countdown=10)
